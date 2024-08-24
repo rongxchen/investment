39c174c8b68babaad33696098c371ce035981b8b
kennychen <chenrongxin20020102@gmail.com>
2024-08-24 19:08:17 +0800

    add mvc config

--- src/main/java/rongxchen/investment/config/MvcConfig.java
+++ src/main/java/rongxchen/investment/config/MvcConfig.java
@@ -1,12 +1,21 @@
 package rongxchen.investment.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import rongxchen.investment.interceptors.LoginInterceptor;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
